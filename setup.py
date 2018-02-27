2237500b817cf3cd4b662f7f85398d0149d2055d
Shaun Martin <shaun@samsite.ca>
2018-02-27 10:54:36 -0500

    Bump version: 0.1.2 → 0.1.3

--- setup.py
+++ setup.py
@@ -50,6 +50,6 @@ setup(
     test_suite='tests',
     tests_require=dev_requirements + requirements,
     url='https://github.com/inhumantsar/python-docker-dynamic-inventory',
-    version='0.1.2',
+    version='0.1.3',
     zip_safe=False,
 )
